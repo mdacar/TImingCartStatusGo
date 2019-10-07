ae8e114ad0b3ba77db674bdc8049f06aaaabbb0a
mdacar <michaeldacar@hotmail.com>
2019-10-06 23:00:12 -0400

    Begin work on making the listener observable

--- connectTcpListener.go
+++ connectTcpListener.go
@@ -2,19 +2,44 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"net"
+	"time"
 )
 
-func main() {
-	fmt.Println("Starting to listen")
-	readerIp := "192.168.1.79:14150"
-
-	conn, _ := net.Dial("tcp", readerIp)
-	for {
-
-		message, _ := bufio.NewReader(conn).ReadString('\n')
-		fmt.Print(message)
-	}
-
+type ConnectTcpListener struct {
+	IsListening   bool
+	LastHeartbeat time.Time
+	IPAddress     string
+	Port          string
 }
+
+func (l ConnectTcpListener) StartListening(ipAddress string, port string) {
+	l.IPAddress = ipAddress
+	l.Port = port
+	conn, err := net.Dial("tcp", ipAddress+":"+port)
+	if err != nil {
+		l.IsListening = false
+		return
+	}
+	l.IsListening = true
+	for {
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+
+	}
+}
+
+func
+
+type ReaderOutput interface
+
+type ReaderHeartbeat struct {
+	Time time.Time
+}
+
+type TagRead struct {
+	TagId     string
+	Timestamp string
+	Antenna   string
+}
+
+type ReaderData string
